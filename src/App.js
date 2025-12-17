ed4646e738af38cf07144a067cc5de5f32e44f7f
johndonga72 <johndonga72@gmail.com>
2025-12-17 14:02:49 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -5,42 +5,19 @@ import ScoreCardOperations from "./components/scoreCardOperations/ScoreCardOpera
 function App() {
   let [score, scoreFun] = useState(100);
   console.log("app component rendering", score);
-
-  // function incrementCounter() {
-  //   console.log("I am incrementing");
-  //   score = score + 1;
-  //   scoreFun(score);
-  //   console.log("updated score", score);
-  // }
-
   function incrementCounter(value) {
     console.log("I am incrementing");
     score = score + value;
     scoreFun(score);
     console.log("updated score", score);
   }
-
-  // function incrementBy5Counter() {
-  //   console.log("I am incrementing");
-  //   score = score + 5;
-  //   scoreFun(score);
-  //   console.log("updated score", score);
-  // }
-
-  // function normalTestFun() {
-  //   console.log("hi hi hi");
-  // }
-
-  //useCall, useMemo, React.memo
   return (
     <div>
       <ScoreCard score={score} />
       <ScoreCardOperations
         incrementScore={incrementCounter}
-      // incrementScoreBy5={incrementBy5Counter}
       />
     </div>
   );
 }
-
 export default App;
